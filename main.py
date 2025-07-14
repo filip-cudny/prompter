be27579fd5fc2f0249f2236a6f8ff2da1f6a0087
Filip Cudny <filip96zx@gmail.com>
2025-07-14 22:57:46 +0200

    feat(mac): set LSUIElement to hide dock icon on macOS
    Hide the application dock icon on macOS by setting LSUIElement to 1 using
    NSBundle.

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 """Entry point for the Prompter application."""
 
 import sys
-
 from app.application import PrompterApp
+from modules.utils.system import is_macos
 
 
 def main():
     """Main entry point."""
     import argparse
+
+    if is_macos():
+        from AppKit import NSBundle
+        info = NSBundle.mainBundle().infoDictionary()
+        info["LSUIElement"] = "1"
 
     parser = argparse.ArgumentParser(description="Prompter PyQt5 Application")
     parser.add_argument("--config", "-c", help="Configuration file path")
